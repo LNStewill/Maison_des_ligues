add907cb021799281298bbb46888aa35eed3a2dc
Nathan Stéwill LOUFOUA <127401929+LNStewill@users.noreply.github.com>
2024-06-17 09:22:46 +0200

    ajout modier_membre

---
+++ controller/update.class.php
@@ -0,0 +1,133 @@
+<?php
+
+class UserUpdate {
+
+    static function insertUser() {
+        require_once "config.inc.php"; # Inclure le fichier de configuration de la base de données
+
+        require_once "connection_bdd.php";
+
+            // Vérifier si le formulaire a été soumis
+            if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+                // Récupérer les données en entrée du formulaire
+                $nom= $_POST['nom'];
+                $prenom = $_POST['prenom'];
+                $age = $_POST['age'];
+                $mail = $_POST["email"];
+                $login = $_POST["login"];
+                $mot_de_passe = $_POST["mot_de_passe"];
+                $ville = $_POST["ville"];
+                #$image_profile = $_FILES['image_profile']['tmp_name'];
+                $image_profile = isset($_POST['use_default_avatar']) ? 'default_avatar.png' : $_FILES['image_profile']['tmp_name'];
+
+                #Verifier si les champs sont remplis
+
+                if (empty($nom) || empty($mail) || empty($login) || empty($mot_de_passe)) {
+                    $_SESSION['errors'][] ="Les champs noms, mails, login, mot_de_passe sont obligatoires";
+                    #print '<p class="warning msg-alert">Tous les champs sont obligatoires</p>';
+                    
+                    // Rediriger vers la page d'inscription
+                    #header("Location: ./index.php");
+                    exit;                       
+    
+                } else {
+
+                    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                        # vérification si le format du mail est correcte
+                       $_SESSION['login_errors'][] ="Veuillez renseigner un mail valide";
+                   }
+   
+                   else {
+                         
+                    // Vérifier si l'utilisateur existe déjà dans la base de données
+                    $_requete_Verif = $connexion->prepare("SELECT * FROM utilisateur WHERE email = ? OR login = ?");
+            
+                    $_requete_Verif->bindParam(1, $mail);
+                    $_requete_Verif->bindParam(2, $login);
+                    $_requete_Verif->execute();
+                
+                    if ($_requete_Verif->rowCount() > 0) {
+                        
+                        // L'utilisateur existe déjà, stocker un message d'erreur dans la session
+                        $_SESSION['errors'][] = "Cette adresse e-mail et ou login est déjà enregistrée. Choisissez en un autre.";
+                        // L'utilisateur existe déjà, afficher un message d'erreur
+                        #print '<p class="warning msg-alert">Cette adresse e-mail est déjà enregistrée. Choisissez une autre adresse e-mail.</p>';
+                    } 
+                    else {
+                        // L'utilisateur n'existe pas, procéder à l'insertion
+
+                        #verification image si tout est ok !
+
+                        if (isset($_FILES['image_profile']) && $_FILES['image_profile']['error'] === UPLOAD_ERR_OK) {
+
+
+                            $photo_name = htmlspecialchars(pathinfo($_FILES["image_profile"]["name"],PATHINFO_FILENAME));
+                            $photo_extension = strtolower(pathinfo($_FILES["image_profile"]["name"], PATHINFO_EXTENSION));
+
+                            # Vérifier l'extension et autoriser une extention
+                            $allowed_extensions = array("jpg", "jpeg", "png");
+
+                            if (!in_array($photo_extension, $allowed_extensions)) {
+                                $errors[] = "L'extension de la photo doit être jpg, jpeg, ou png.";
+                            }
+
+                            # Vérifier la taille maximale (2 Mo ici, mais tu peux ajuster selon tes besoins)
+                            $max_size = 2 * 1024 * 1024; # 2 Mo
+                            if ($_FILES["image_profile"]["size"] > $max_size) {
+                                $errors[] = "La taille de la photo ne doit pas dépasser 2 Mo.";
+                            }
+
+                            #renommer la photo pour éviter l'enregistrement du meme nom afin de bien gerer l'affichage
+
+                            $photo_name = $photo_name.'_'.date("Ymd_His").'.'. $photo_extension;
+
+                            $image_profile = $photo_name;
+
+                            $motDePasseHash = password_hash($mot_de_passe, PASSWORD_DEFAULT);
+                
+                            // Préparer la requête SQL pour insérer les données dans la base de données
+                            $requete = $connexion->prepare("INSERT INTO utilisateur (nom, prenom, age, email, login, mot_de_passe, ville, image_profile) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+
+                            // Binder les paramètres
+                            $requete->bindParam(1, $nom);
+                            $requete->bindParam(2, $prenom);
+                            $requete->bindParam(3, $age);
+                            $requete->bindParam(4, $mail);
+                            $requete->bindParam(5, $login);
+                            $requete->bindParam(6, $motDePasseHash);
+                            $requete->bindParam(7, $ville);
+                            $requete->bindParam(8, $image_profile);
+
+                            // Exécuter la requête
+                            $requete->execute();
+                            #Stocker dans le repertoire le fichier temporaire uploadé
+                            $uploads = "./../uploads/profile/";
+                            move_uploaded_file($_FILES["image_profile"]["tmp_name"], $uploads . $photo_name);
+
+                            // Stocker les variables pour la page de confirmation d'inscription
+                            // Stockez les informations dans des variables de session
+                            $_SESSION['nom'] = $nom;
+                            $_SESSION['prenom'] = $prenom;
+                            $_SESSION['email'] = $mail;
+                            $_SESSION['ville'] = $ville;
+                            $_SESSION['image_profile'] = $uploads . $photo_name;
+
+                            // Rediriger vers la page de connexion (ou toute autre page souhaitée)
+                            header("Location: ../src/confirmation_inscription.php");
+
+                            exit;
+
+                        } else {
+                            $_SESSION['errors'][] = "Erreur lors du téléchargement de la photo.";
+                        }
+                    }
+                }
+                // Fermer la connexion
+                $connexion = null;
+            }
+        }    
+    }
+}
+
+// Utilisation
+UserUpdate::insertUser();
---
+++ src/modifier_membre.php
@@ -0,0 +1,82 @@
+<?php
+    include_once __DIR__."./../controller/header.inc.php";
+?>
+
+    <section class="content">
+        <h1>
+            MAISON DES LIGUES TOUS LES SPORTS
+        </h1>
+    </section> 
+
+    <section class="form_container">
+        <h1>
+            Inscrivez vous
+        </h1>
+
+        <?php
+        
+        require_once __DIR__."./../controller/register.class.php";
+
+        if (isset($_SESSION['errors'])) {
+            foreach ($_SESSION['errors'] as $error) {
+                echo '<p class="warning msg-alert">' . $error . '</p>';
+            }
+            unset($_SESSION['errors']); // Supprimez les erreurs de la session
+        }
+    ?>
+
+    <br>
+        <form method="post" action="<?php print $_SERVER["PHP_SELF"]; ?>" id="registration-form" class="registration-form" enctype="multipart/form-data">
+        
+        <fieldset class="container">
+
+            <legend>Créer votre compte</legend>
+
+            <label for="nom">Nom</label>
+            <input type="text" id="name" name="nom" placeholder="Votre nom" aria-required="true">
+            
+            <label for="prenom">Prénom</label>
+            <input type="text" id="prenom" name="prenom" placeholder="Votre prénom" aria-required="true">
+            
+            <label for="age">Age</label>
+            <input type="number" min="0" max="130" id="age" name="age" placeholder="Votre âge" aria-required="true">
+            
+            <label for="email">Mail</label>
+            <input type="email" id="email" name="email" placeholder="Votre mail" required aria-required="true">
+            
+            <label for="login">Identifiant</label>
+            <input type="text" id="login" name="login" placeholder="Votre identifiant" required aria-required="true">
+            
+            <label for="mot_de_passe">Choisissez votre mot de passe </label>
+            <input type="password" id="mot_de_passe" name="mot_de_passe" required aria-required="true">
+            
+            <label for="ville">Ville</label>
+            <input type="text" id="ville" name="ville" placeholder="Votre ville" required aria-required="true">
+
+            <label for="use_default_avatar">
+            <input type="checkbox" id="use_default_avatar" name="use_default_avatar">
+            Utiliser un avatar par défaut
+        </label>
+            
+            <label for="image">Telecharger votre photo</label>
+            <input type="file" id="image_profile" name="image_profile" accept="image/*" required aria-required="true">
+            
+            <hr>
+            <input type="submit" class="registerbtn" name="inscription" value="CRÉER UN COMPTE">
+
+        </fieldset>                
+        </form>
+
+        <br>
+
+        <form action="connexion_user.php">
+            <button>
+                Déjà inscrit ?  Cliquer pour se connecter
+            </button>
+        </form>
+    </section>
+
+
+<?php
+    include_once __DIR__."./../includes/footer.inc.php";
+?>
